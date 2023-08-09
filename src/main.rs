d496eb1166077938c1efe31bd485741710ebedcb
joker53-1 <1205637346@qq.com>
2023-08-09 12:53:30 +0000

    first commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::fs::File;
+use std::io::Read;
+
+use memmap2::Mmap;
+
+fn main() {
+    let mut file = File::open("test").unwrap();
+
+    let mut contents = Vec::new();
+    file.read_to_end(&mut contents).unwrap();
+
+    
+    let mmap = unsafe { Mmap::map(&file).unwrap() };
+
+    for i in &contents {
+        println!("{}", i);
+    }
+    assert_eq!(&contents[..], &mmap[..]);
+}
